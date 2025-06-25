1cbd798d9e49b8df75942a1def605f64611dae03
pavankumar281 <pavaneerla07@gmail.com>
2025-06-25 15:27:20 +0530

    dom

---
+++ DOM.js
@@ -0,0 +1,19 @@
+document = "DOM in javascript";
+ 
+document.getElementById("onepice").innerHTML = " OREVA: MONKEY D LUFFY!!";
+
+document.getElementById("OP").innerHTML = "I AM THE CAPTAIN OF STRW HAT PAIRETS";
+
+document.getElementById("OP").style.color = "blue";
+
+document.getElementById("OP").style.fontSize = "38px"
+
+
+document.getElementsByClassName("ds")[0].innerHTML="I AM GOING TO BECOME THE KING OF THE HELL";
+document.getElementsByClassName("ds")[0].style.color="red";
+document.getElementsByClassName("ds")[0].style.fontSize="50px";
+document.getElementsByTagName("h1")[1].innerHTML="OREVA: ROVANOVA ZORO";
+document.getElementsByTagName("h1")[1].style.color="green";
+document.body.style.backgroundColor ="lightblue"
+
+document.getElementsByClassName("luffy").innerHTML = " https://one-piece.com/op/links_eng/assets/img/PC_OP_XRAMS_LP_animeKV.jpg";
